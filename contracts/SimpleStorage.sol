024880d224132d5efd7ffd6204a6f9dda74d6279
dandwang <1016403559@QQ.COM>
2025-03-21 17:59:41 +0800

    second commit

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -5,6 +5,36 @@ contract SimpleStorage {
     //boolean uint int address bytes
 
     // bytes32 data = "王";
-    bytes32 data1 = "abc";
-    uint8 num = -0;
+    // bytes32 data1 = "abc";
+    // uint8 num = -0;
+    uint256 public  favoriteNumber;
+    // uint256 public brotherNumber=3;
+    
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public  peopleList;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function addPeople(string memory name,uint256 _favoriteNumber) public{
+        peopleList.push(People(_favoriteNumber,name));
+        nameToFavoriteNumber[name]=_favoriteNumber;
+    }
+
+    // function retrieve() public view returns (uint256) {
+
+    //     return favoriteNumber;
+    // }
+
+    // function pure_test() public  pure returns (bool){
+    //     return false;
+    // }
+
 }
