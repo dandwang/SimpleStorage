934b5c24728214086b4a94ae2a19e9d7512581ce
dandwang <1016403559@QQ.COM>
2025-03-21 14:46:13 +0800

    firstcommit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
+
+contract SimpleStorage {
+    //boolean uint int address bytes
+
+    // bytes32 data = "王";
+    bytes32 data1 = "abc";
+    uint8 num = -0;
+}
